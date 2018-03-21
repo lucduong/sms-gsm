9e80fcc1983328c187c2f275226e9e69c63791eb
Luc <luc@ltv.vn>
2018-03-21 15:56:04 +0700

    Modify Message.ts

--- src/Message.ts
+++ src/Message.ts
@@ -1,24 +1,8 @@
-export class Message{
-    private _message:string;
-    private _phoneNumber:string;
-    constructor(message:string,phoneNumber){
-        this._message=message;
-        this._phoneNumber=phoneNumber;
-    }
-
-    get message():string{
-        return this._message;
-    }
-
-    set message(val:string){
-        this._message=val;
-    }
-
-    get phoneNumber():string{
-        return this._phoneNumber;
-    }
-
-    set phoneNumber(val:string){
-        this._message=val;
-    }
+export class Message {
+  message: String;
+  phone: String;
+  constructor(message: String, phone: String) {
+    this.message = message;
+    this.phone = phone;
+  }
 }
