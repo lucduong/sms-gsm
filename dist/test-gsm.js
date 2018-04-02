a3eaa379ad5133fd7975c7cf917406c631a9288e
unknown <blackangel1306@gmail.com>
2018-04-02 09:19:51 +0700

    Read mesage

--- dist/test-gsm.js
+++ dist/test-gsm.js
@@ -5,11 +5,6 @@ var Message_1 = require("./Message");
 var message = new Message_1.Message("Test goi tin nhan", "0938256706");
 var _ = require('lodash');
 var testPort = new TestPort_1.TestPort("/dev/ttyUSB0");
-var users = [
-    { 'user': 'barney', 'age': 36, 'active': true },
-    { 'user': 'fred', 'age': 40, 'active': false },
-    { 'user': 'pebbles', 'age': 1, 'active': true }
-];
-var tmp = _.find(users, { 'age': 1, 'active': true });
-console.log(tmp);
+testPort.deleteAllSMS();
+testPort.readMessage();
 //# sourceMappingURL=test-gsm.js.map
